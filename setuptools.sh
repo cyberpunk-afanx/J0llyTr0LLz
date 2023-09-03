048f9737c81c0cee2642d2ad2db03702a138a14b
AFANX <119620395+maccept@users.noreply.github.com>
2023-09-03 12:21:01 +0300

    Add files via upload

---
+++ setuptools.sh
@@ -0,0 +1,10 @@
+sudo apt install python3-pyqt5
+sudo apt install upx
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential
+gem install seccomp-tools
+sudo apt install gcc ruby-dev
+python3 -m pip install --upgrade pip
+pip install hashlib
+pip install zlib-state
+pip install python-magic
+pip install jsonlib
